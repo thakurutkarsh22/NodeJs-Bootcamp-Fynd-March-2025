e475549c13ef428df1b414368e6eeead1a1487c7
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2025-03-16 13:18:06 +0530

    validator for DB

--- 4.BlogsBackendProject/Database/Models/UserModel.js
+++ 4.BlogsBackendProject/Database/Models/UserModel.js
@@ -1,10 +1,10 @@
 const { Schema, default: mongoose} = require("mongoose");
-
+const validator = require("validator");
 
 const userSchema = new Schema({
-    email: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, validate: (value) => validator.isEmail(value)},
     password: {type: String, required: true, minlength: 8},
-    username: {type: String, required: true, minlength: 5, unique: true}
+    username: {type: String, required: true, minlength: 5, unique: true, validate: (value) => validator.isAlphanumeric(value)}
 }, {timestamps: true});
 
 
